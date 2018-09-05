e69c57d8cf09a8e0d38d80c258572509d3b6c976
Robert Smargiassi <rps52@drexel.edu>
2018-09-05 17:36:32 -0400

    branches

--- rust/functions/src/main.rs
+++ rust/functions/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
+    
+    another_function(5, 6);
+    let x = plus_one(5);
+
+    println!("The value of x is: {}", x);
+}
+
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
